1676852ac2af402c8f3810251417e15b0f086f6b
Jan Lehigh <janlehigh461437@gmail.com>
2024-04-03 09:44:33 -0600

    Add test for driver? and drive method in passenger class

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -34,6 +34,14 @@ RSpec.describe Passenger do
             expect(taylor.age).to eq(12)
         end
 
+        it "has an age" do
+            charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+            taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+            expect(charlie.age).to eq(18)
+            expect(taylor.age).to eq(12)
+        end
+
     end
 
     describe '#adult?' do
@@ -43,6 +51,22 @@ RSpec.describe Passenger do
 
             expect(charlie.adult?).to be true
             expect(taylor.adult?).to be false
+        end
+    end
+
+    describe '#driver?' do
+        it 'can determine if passenger is a driver' do
+            charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+            taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+            expect(charlie.driver?).to be false
+            expect(taylor.driver?).to be false
+
+            charlie.drive
+            taylor.drive
+
+            expect(charlie.driver?).to be true
+            expect(taylor.driver?).to be false 
         end
     end
 end
