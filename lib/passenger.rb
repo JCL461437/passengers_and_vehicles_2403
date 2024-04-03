c2b75342baf2590d788e5e104f91022257b9eb22
Jan Lehigh <janlehigh461437@gmail.com>
2024-04-03 09:44:59 -0600

    Add methods for driver? and drive in Passenger class

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -5,12 +5,21 @@ class Passenger
     def initialize(passenger_details)
         @name = passenger_details["name"]
         @age = passenger_details["age"].to_i
+        @driver = false
     end
 
     def adult?
         @age >= 18
     end
 
-    
+    def driver?
+        @driver
+    end
+
+    def drive
+        if adult? 
+            @driver = true
+        end
+    end
 
 end
