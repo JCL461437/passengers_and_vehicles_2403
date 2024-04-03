9df8871c60625df048e9e0303f4033dce2b59b52
Jan Lehigh <janlehigh461437@gmail.com>
2024-04-03 14:23:41 -0600

    Add refactored park_spec.rb file

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -12,21 +12,26 @@ RSpec.describe Park do
    
     describe '#initialize' do
         it 'can initialize' do
-            yosemite = Park.new(150)
+            yosemite = Park.new("Yosemite", 20.0)
 
             expect(yosemite).to be_an_instance_of(Park)
         end
         
-        it 'has an admission price' do
-            yosemite = Park.new(150)
+        it 'has an name' do
+            yosemite = Park.new("Yosemite", 20.0)
 
-            expect(yosemite.admission).to eq (150)
+            expect(yosemite.name).to eq ("Yosemite")
+        end
+
+        it 'has an admission price' do
+            yosemite = Park.new("Yosemite", 20.0)
+
+            expect(yosemite.admission).to eq (20.0)
         end
     end
 
     describe '#add_vehicle_to_park' do
     
-        
         it 'can add vehicles' do
             vehicle_1 = Vehicle.new("2001", "Honda", "Civic")
             vehicle_2 = Vehicle.new("2020", "Honda", "Pilot")
